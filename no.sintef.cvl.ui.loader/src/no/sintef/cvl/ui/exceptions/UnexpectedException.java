5183b8f968dce621f71d715127b0ab2ce812837b
vassik <Anatoly.Vasilevskiy@sintef.no>
2013-05-24 13:28:31 +0200

    exceptions dir, why it is not in repo still...

---
+++ no.sintef.cvl.ui.loader/src/no/sintef/cvl/ui/exceptions/UnexpectedException.java
@@ -0,0 +1,12 @@
+package no.sintef.cvl.ui.exceptions;
+
+public class UnexpectedException extends AbstractError {
+
+	private static final long serialVersionUID = -2346363414529294965L;
+
+	public UnexpectedException(String msg) {
+		super(msg);
+	}
+
+
+}
