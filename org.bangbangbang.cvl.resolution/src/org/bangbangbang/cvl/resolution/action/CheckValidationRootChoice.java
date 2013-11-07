bf70d4cb9dd440484ff2aa6b2de539fe94ddae5e
DaisukeShimbara <bangbangbang.org@gmail.com>
2013-11-07 17:27:48 +0000

    Add checking GroupMultiplicity

--- org.bangbangbang.cvl.resolution/src/org/bangbangbang/cvl/resolution/action/CheckValidationRootChoice.java
+++ org.bangbangbang.cvl.resolution/src/org/bangbangbang/cvl/resolution/action/CheckValidationRootChoice.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.TreeMap;
 
 import org.bangbangbang.cvl.ChoiceResolutuion;
 import org.bangbangbang.cvl.ConfigurableUnit;
@@ -21,6 +22,8 @@ import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.edit.domain.EditingDomain;
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.StructuredSelection;
@@ -41,6 +44,7 @@ public class CheckValidationRootChoice implements IHandler {
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		editorPart = HandlerUtil.getActiveEditorChecked(event);
 		viewer = (TreeViewer) ((IViewerProvider) editorPart).getViewer();
+		Map<String, VSpecResolution> result = new TreeMap<String, VSpecResolution>();
 
 		if (!(editorPart instanceof CvlResolutionEditor)) {
 			return null;
@@ -64,8 +68,16 @@ public class CheckValidationRootChoice implements IHandler {
 			return null;
 		}
 
-		Map<String, VSpecResolution> result = traverseResolutionTree(
-				editingDomain, editorPart, viewer, targetRootChoice);
+		result = traverseResolutionTree(editingDomain, editorPart, viewer,
+				targetRootChoice, result);
+
+		if (result.size() == 0) {
+			Dialog dialog = new MessageDialog(PlatformUI.getWorkbench()
+					.getModalDialogShellProvider().getShell(), "No Violation",
+					null, null, 0, null, 0);
+			dialog.open();
+			return null;
+		}
 
 		ListDialog dialog = new ListDialog(PlatformUI.getWorkbench()
 				.getModalDialogShellProvider().getShell());
@@ -85,6 +97,7 @@ public class CheckValidationRootChoice implements IHandler {
 			}
 
 		});
+
 		if (dialog.open() == Window.OK) {
 			Object[] selection = dialog.getResult();
 			if (selection.length > 0) {
@@ -99,12 +112,12 @@ public class CheckValidationRootChoice implements IHandler {
 
 	public static Map<String, VSpecResolution> traverseResolutionTree(
 			EditingDomain editDomain, IEditorPart editPart, TreeViewer viewer,
-			ChoiceResolutuion root) {
-		Map<String, VSpecResolution> result = new HashMap<String, VSpecResolution>();
+			ChoiceResolutuion root, Map<String, VSpecResolution> result) {
 		// for Root Choice resolution, check group multiplicity
 		// and check constraints
+		checkGroupMultiplicity(root, result, root);
 
-		// traverse tree node
+		// traverse tree of resolution
 		for (TreeIterator<EObject> iterator = root.eAllContents(); iterator
 				.hasNext();) {
 			EObject obj = iterator.next();
@@ -113,9 +126,11 @@ public class CheckValidationRootChoice implements IHandler {
 			}
 			VSpecResolution target = (VSpecResolution) obj;
 
-			// Check : Is target have VInstance child?
-			EList<VSpecResolution> children = target.getChild();
+			// Check : Is target have VInstance child? for checking Instance
+			// multiplicity
 			Map<VClassifier, Integer> vclassifiers = new HashMap<VClassifier, Integer>();
+
+			EList<VSpecResolution> children = target.getChild();
 			for (Iterator<VSpecResolution> childrenIterator = children
 					.iterator(); childrenIterator.hasNext();) {
 				VSpecResolution res = childrenIterator.next();
@@ -155,9 +170,52 @@ public class CheckValidationRootChoice implements IHandler {
 
 			}
 
+			// if target has group multiplicity, check choice decision count of
+			// target children
+			checkGroupMultiplicity(root, result, target);
+
 		}
 
 		return result;
+	}
+
+	private static void checkGroupMultiplicity(ChoiceResolutuion root,
+			Map<String, VSpecResolution> result, VSpecResolution target) {
+		if ((target instanceof ChoiceResolutuion && ((ChoiceResolutuion) target)
+				.getResolvedChoice().getGroupMultiplicity() != null)
+				|| (target instanceof VInstance && ((VInstance) target)
+						.getType().getGroupMultiplicity() != null)) {
+			MultiplicityInterval interval = null;
+			if (target instanceof ChoiceResolutuion) {
+				interval = ((ChoiceResolutuion) target).getResolvedChoice()
+						.getGroupMultiplicity();
+			} else if (target instanceof VInstance) {
+				interval = ((VInstance) target).getType()
+						.getGroupMultiplicity();
+			} else {
+				assert (false);
+			}
+			int count = 0;
+			for (Iterator<VSpecResolution> childrenIterator = target.getChild()
+					.iterator(); childrenIterator.hasNext();) {
+				VSpecResolution child = childrenIterator.next();
+				if (child instanceof ChoiceResolutuion
+						&& ((ChoiceResolutuion) child).isDecision()) {
+					count++;
+				}
+			}
+			if (interval.getLower() > count || interval.getUpper() < count) {
+				// Violation
+				result.put(
+						String.valueOf(result.size() + 1)
+								+ " GroupMultiplicity Violation ["
+								+ String.valueOf(interval.getLower()) + ".."
+								+ String.valueOf(interval.getUpper())
+								+ "] at Product:" + root.getName()
+								+ " Element:"
+								+ target.getResolvedVSpec().getName(), target);
+			}
+		}
 	}
 
 	@Override
