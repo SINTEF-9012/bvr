947d2da40009fe2ed9cad173eefc307b23cc6e04
DaisukeShimbara <bangbangbang.org@gmail.com>
2013-11-21 14:36:26 +0000

    Disable deleting child link.

--- org.bangbangbang.cvl.diagram/src/org/bangbangbang/cvl/diagram/edit/policies/VSpecChildItemSemanticEditPolicy.java
+++ org.bangbangbang.cvl.diagram/src/org/bangbangbang/cvl/diagram/edit/policies/VSpecChildItemSemanticEditPolicy.java
@@ -6,7 +6,9 @@ import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.gef.Request;
 import org.eclipse.gef.commands.Command;
+import org.eclipse.gef.commands.UnexecutableCommand;
 import org.eclipse.gmf.runtime.common.core.command.CommandResult;
 import org.eclipse.gmf.runtime.emf.type.core.commands.DestroyReferenceCommand;
 import org.eclipse.gmf.runtime.emf.type.core.requests.DestroyReferenceRequest;
@@ -25,23 +27,24 @@ public class VSpecChildItemSemanticEditPolicy extends
 	}
 
 	/**
-	 * @generated
+	 * @generated NOT
 	 */
 	protected Command getDestroyReferenceCommand(DestroyReferenceRequest req) {
-		return getGEFWrapper(new DestroyReferenceCommand(req) {
-			protected CommandResult doExecuteWithResult(
-					IProgressMonitor progressMonitor, IAdaptable info)
-					throws ExecutionException {
-				EObject referencedObject = getReferencedObject();
-				Resource resource = referencedObject.eResource();
-				CommandResult result = super.doExecuteWithResult(
-						progressMonitor, info);
-				if (resource != null) {
-					resource.getContents().add(referencedObject);
-				}
-				return result;
-			}
-		});
+		return UnexecutableCommand.INSTANCE;
+//		return getGEFWrapper(new DestroyReferenceCommand(req) {
+//			protected CommandResult doExecuteWithResult(
+//					IProgressMonitor progressMonitor, IAdaptable info)
+//					throws ExecutionException {
+//				EObject referencedObject = getReferencedObject();
+//				Resource resource = referencedObject.eResource();
+//				CommandResult result = super.doExecuteWithResult(
+//						progressMonitor, info);
+//				if (resource != null) {
+//					resource.getContents().add(referencedObject);
+//				}
+//				return result;
+//			}
+//		});
 	}
 
 }
