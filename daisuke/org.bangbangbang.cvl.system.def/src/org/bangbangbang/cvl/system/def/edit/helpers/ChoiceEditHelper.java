7db95b3ea66bf80603538a2d9b85986f97d99fdb
DaisukeShimbara <bangbangbang.org@gmail.com>
2013-12-11 09:12:19 +0100

    Override getDestroyElementCommand for disable delete root choice

--- daisuke/org.bangbangbang.cvl.system.def/src/org/bangbangbang/cvl/system/def/edit/helpers/ChoiceEditHelper.java
+++ daisuke/org.bangbangbang.cvl.system.def/src/org/bangbangbang/cvl/system/def/edit/helpers/ChoiceEditHelper.java
@@ -1,7 +1,25 @@
 package org.bangbangbang.cvl.system.def.edit.helpers;
 
+import org.bangbangbang.cvl.ConfigurableUnit;
+import org.eclipse.gmf.runtime.common.core.command.ICommand;
+import org.eclipse.gmf.runtime.common.core.command.UnexecutableCommand;
+import org.eclipse.gmf.runtime.emf.type.core.requests.DestroyElementRequest;
+
 /**
  * @generated
  */
 public class ChoiceEditHelper extends CVLMetamodelBaseEditHelper {
+	/**
+	 * Change: override getDestroyElementCommand for disable delete root choice
+	 * @generated NOT
+	 */
+	@Override
+	protected ICommand getDestroyElementCommand(DestroyElementRequest req) {
+		if (req.getElementToDestroy().eContainer() instanceof ConfigurableUnit) {
+			// Prevents from removing the figure
+			return UnexecutableCommand.INSTANCE;
+		}
+		return super.getDestroyElementCommand(req);
+	}
+
 }
