5904e29fc1f9fac30eef6f756bc4ce698f9d9c19
DaisukeShimbara <bangbangbang.org@gmail.com>
2013-12-11 10:47:08 +0100

    Disable only link delete

--- daisuke/org.bangbangbang.cvl.system.def/src/org/bangbangbang/cvl/system/def/edit/policies/VSpecChildItemSemanticEditPolicy.java
+++ daisuke/org.bangbangbang.cvl.system.def/src/org/bangbangbang/cvl/system/def/edit/policies/VSpecChildItemSemanticEditPolicy.java
@@ -7,6 +7,7 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.gef.commands.Command;
+import org.eclipse.gef.commands.UnexecutableCommand;
 import org.eclipse.gmf.runtime.common.core.command.CommandResult;
 import org.eclipse.gmf.runtime.emf.type.core.commands.DestroyReferenceCommand;
 import org.eclipse.gmf.runtime.emf.type.core.requests.DestroyReferenceRequest;
@@ -25,23 +26,25 @@ public class VSpecChildItemSemanticEditPolicy extends
 	}
 
 	/**
-	 * @generated
+	 * Change: Desable delete only link
+	 * @generated NOT
 	 */
 	protected Command getDestroyReferenceCommand(DestroyReferenceRequest req) {
-		return getGEFWrapper(new DestroyReferenceCommand(req) {
-			protected CommandResult doExecuteWithResult(
-					IProgressMonitor progressMonitor, IAdaptable info)
-					throws ExecutionException {
-				EObject referencedObject = getReferencedObject();
-				Resource resource = referencedObject.eResource();
-				CommandResult result = super.doExecuteWithResult(
-						progressMonitor, info);
-				if (resource != null) {
-					resource.getContents().add(referencedObject);
-				}
-				return result;
-			}
-		});
+		return UnexecutableCommand.INSTANCE;
+//		return getGEFWrapper(new DestroyReferenceCommand(req) {
+//			protected CommandResult doExecuteWithResult(
+//					IProgressMonitor progressMonitor, IAdaptable info)
+//					throws ExecutionException {
+//				EObject referencedObject = getReferencedObject();
+//				Resource resource = referencedObject.eResource();
+//				CommandResult result = super.doExecuteWithResult(
+//						progressMonitor, info);
+//				if (resource != null) {
+//					resource.getContents().add(referencedObject);
+//				}
+//				return result;
+//			}
+//		});
 	}
 
 }
