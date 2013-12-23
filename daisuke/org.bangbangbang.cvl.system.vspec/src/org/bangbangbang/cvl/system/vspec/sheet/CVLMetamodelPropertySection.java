24cd2d1a775577ba56b98855c4ea82390c8654f9
DaisukeShimbara <bangbangbang.org@gmail.com>
2013-12-23 15:20:55 +0100

    Import ocl property editor

--- daisuke/org.bangbangbang.cvl.system.vspec/src/org/bangbangbang/cvl/system/vspec/sheet/CVLMetamodelPropertySection.java
+++ daisuke/org.bangbangbang.cvl.system.vspec/src/org/bangbangbang/cvl/system/vspec/sheet/CVLMetamodelPropertySection.java
@@ -1,10 +1,14 @@
 package org.bangbangbang.cvl.system.vspec.sheet;
 
+import org.bangbangbang.cvl.diagram.custom.property.CustomPropertySource;
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.edit.provider.IItemPropertySource;
 import org.eclipse.gmf.tooling.runtime.sheet.DefaultPropertySection;
+import org.eclipse.ui.views.properties.IPropertySource;
 import org.eclipse.ui.views.properties.IPropertySourceProvider;
 
 /**
- * @generated
+ * @generated NOT
  */
 public class CVLMetamodelPropertySection extends DefaultPropertySection
 		implements IPropertySourceProvider {
@@ -19,4 +23,21 @@ public class CVLMetamodelPropertySection extends DefaultPropertySection
 		return selected;
 	}
 
+	/**
+	 * @generated NOT
+	 */
+	@Override
+	public IPropertySource getPropertySource(Object object) {
+		AdapterFactory af = getAdapterFactory(object);
+		IItemPropertySource ips = null;
+		if (af != null) {
+			ips = (IItemPropertySource) af.adapt(object,
+					IItemPropertySource.class);
+		}
+		if (object != null && ips != null) {
+			return new CustomPropertySource(object, ips);
+		} else {
+			return super.getPropertySource(object);
+		}
+	}
 }
