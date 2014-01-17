18f41711e6fe9417474042d59b451ca8bb5f157f
vassik <Anatoly.Vasilevskiy@sintef.no>
2014-01-17 16:08:07 +0100

    test cases are all back!

--- kepler4.3.1/no.sintef.cvl.thirdparty/src/no/sintef/cvl/thirdparty/common/Constants.java
+++ kepler4.3.1/no.sintef.cvl.thirdparty/src/no/sintef/cvl/thirdparty/common/Constants.java
@@ -2,7 +2,7 @@ package no.sintef.cvl.thirdparty.common;
 
 public final class Constants {
 	
-	public static final String PLUGIN_ID_CVL_LAUNCHER = "no.sintef.cvl.ui.editor.eclipselauncher";
+	public static final String PLUGIN_ID_CVL_LAUNCHER = "no.sintef.bvr.ui.editor.mvc.realization";
 	public static final String PLUGIN_ID_THIRDPARTY = "no.sintef.cvl.thirdparty";
 
 }
