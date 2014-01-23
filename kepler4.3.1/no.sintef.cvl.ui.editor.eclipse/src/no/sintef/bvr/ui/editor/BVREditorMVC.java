d4b867b1bf5a46bd390a9ed24dbbae9addce9ef5
DaisukeShimbara <bangbangbang.org@gmail.com>
2014-01-23 14:06:59 +0100

    Move thread creation and run from init() to create PartControl for avoiding null pointer exception with variable frame.

--- kepler4.3.1/no.sintef.cvl.ui.editor.eclipse/src/no/sintef/bvr/ui/editor/BVREditorMVC.java
+++ kepler4.3.1/no.sintef.cvl.ui.editor.eclipse/src/no/sintef/bvr/ui/editor/BVREditorMVC.java
@@ -32,73 +32,49 @@ import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.part.EditorPart;
 import org.eclipse.ui.part.FileEditorInput;
 
-public abstract class BVREditorMVC extends EditorPart implements ISaveablePart, IResourceChangeListener, BVRNotifier {
+public abstract class BVREditorMVC extends EditorPart implements ISaveablePart,
+		IResourceChangeListener, BVRNotifier {
 
 	JTabbedPane pane = new JTabbedPane();
 	protected JLayeredPane x = new JLayeredPane();
 	private Frame frame;
 	private Composite composite;
-	
-	public BVREditorMVC(){
+
+	public BVREditorMVC() {
 		super();
 		ResourcesPlugin.getWorkspace().addResourceChangeListener(this);
 	}
-	
+
 	@Override
 	public void resourceChanged(IResourceChangeEvent event) {
-		
+
 	}
-	
+
 	protected CVLView v;
 	protected CVLModel m;
 	protected String filename;
-	
+
 	FileEditorInput fileinput;
 
 	@Override
-	public void init(IEditorSite site, IEditorInput input) throws PartInitException {
-		final BVRNotifier ep = this;
-		
+	public void init(IEditorSite site, IEditorInput input)
+			throws PartInitException {
+
 		setSite(site);
 		setInput(input);
 		fileinput = (FileEditorInput) input;
 		filename = fileinput.getFile().getLocation().toString();
 		setContentDescription("CVLEditor:" + filename);
 		setTitle();
-				
-		new Thread(){
-			public void run(){
-				if(fileinput!= null){
-					try {
-						UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-			        	m = CVLModelSingleton.getModel(new File(filename));
-			        	if (m != null) {
-			        		JApplet a = new JApplet(); 
-			        		createView(ep);
-			        		// "The first child of the embedded frame must be a heavyweight component."
-			        		frame.add(a);
-			        		setContents(a);
-			        		int w = (int)frame.getBounds().getWidth();
-			        		int h = (int)frame.getBounds().getHeight();
-			        		a.setSize(w, h);
-			        		frame.revalidate();
-			        		frame.repaint();
-			        		ep.notifyProbeDirty();
-			        	}
-					} catch(Exception e){
-						e.printStackTrace();
-						System.err.println("Cannot open file" + e + " " + e.getMessage());						
-					}
-		        }
-			}
-		}.start();
+		
+
 	}
 
-	abstract public void setTitle() ;
-	
-	abstract public void setContents(JApplet a) ;
+	abstract public void setTitle();
 
-	abstract public void createView(final BVRNotifier ep) ;
+	abstract public void setContents(JApplet a);
+
+	abstract public void createView(final BVRNotifier ep);
 
 	@Override
 	public void doSave(IProgressMonitor monitor) {
@@ -108,23 +84,19 @@ public abstract class BVREditorMVC extends EditorPart implements ISaveablePart, 
 			notifyProbeDirty();
 			fileinput.getFile().refreshLocal(IResource.DEPTH_ZERO, null);
 		} catch (final IOException e) {
-			new Thread(){
-				public void run(){
+			new Thread() {
+				public void run() {
 					JOptionPane.showMessageDialog(frame,
-						"Error Saving: " + e.getMessage(),
-						"Saving Failed",
-						JOptionPane.ERROR_MESSAGE
-					);
+							"Error Saving: " + e.getMessage(), "Saving Failed",
+							JOptionPane.ERROR_MESSAGE);
 				}
 			}.start();
 		} catch (final CoreException e) {
-			new Thread(){
-				public void run(){
+			new Thread() {
+				public void run() {
 					JOptionPane.showMessageDialog(frame,
-						"Error Saving: " + e.getMessage(),
-						"Saving Failed",
-						JOptionPane.ERROR_MESSAGE
-					);
+							"Error Saving: " + e.getMessage(), "Saving Failed",
+							JOptionPane.ERROR_MESSAGE);
 				}
 			}.start();
 		}
@@ -133,14 +105,14 @@ public abstract class BVREditorMVC extends EditorPart implements ISaveablePart, 
 	@Override
 	public void doSaveAs() {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	@Override
 	public boolean isDirty() {
-		if(v != null){
+		if (v != null) {
 			return v.isDirty();
-		}else{
+		} else {
 			return false;
 		}
 	}
@@ -153,21 +125,51 @@ public abstract class BVREditorMVC extends EditorPart implements ISaveablePart, 
 	@Override
 	public void createPartControl(Composite parent) {
 		composite = new Composite(parent, SWT.EMBEDDED | SWT.NO_BACKGROUND);
-        FillLayout layout = new FillLayout();
-        composite.setLayout(layout);
-        frame = SWT_AWT.new_Frame(composite);
+		FillLayout layout = new FillLayout();
+		composite.setLayout(layout);
+		frame = SWT_AWT.new_Frame(composite);
+		
+		final BVRNotifier ep = this;
+
+		new Thread() {
+			public void run() {
+				if (fileinput != null) {
+					try {
+						UIManager.setLookAndFeel(UIManager
+								.getSystemLookAndFeelClassName());
+						m = CVLModelSingleton.getModel(new File(filename));
+						if (m != null) {
+							JApplet a = new JApplet();
+							createView(ep);
+							// "The first child of the embedded frame must be a heavyweight component."
+							frame.add(a);
+							setContents(a);
+							int w = (int) frame.getBounds().getWidth();
+							int h = (int) frame.getBounds().getHeight();
+							a.setSize(w, h);
+							frame.revalidate();
+							frame.repaint();
+							ep.notifyProbeDirty();
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+						System.err.println("Cannot open file" + e + " "
+								+ e.getMessage());
+					}
+				}
+			}
+		}.start();
 	}
 
 	@Override
 	public void setFocus() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void notifyProbeDirty() {
 		firePropertyChange(ISaveablePart.PROP_DIRTY);
 	}
-
 
 }
