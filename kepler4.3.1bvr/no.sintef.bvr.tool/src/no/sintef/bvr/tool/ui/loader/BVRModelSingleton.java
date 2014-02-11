f7f8a3446486613ca04a6ab1ae0da2ce0425d04c
mjoha <mjoha@SINTEFPC3339.sintef.no>
2014-02-11 11:23:49 +0100

    Implemented model closing when last editor is closed.

--- kepler4.3.1bvr/no.sintef.bvr.tool/src/no/sintef/bvr/tool/ui/loader/BVRModelSingleton.java
+++ kepler4.3.1bvr/no.sintef.bvr.tool/src/no/sintef/bvr/tool/ui/loader/BVRModelSingleton.java
@@ -1,21 +1,46 @@
 package no.sintef.bvr.tool.ui.loader;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+
+import org.eclipse.ui.IWorkbenchPart;
 
 public class BVRModelSingleton {
 	static Map<File, BVRModel> loaded = new HashMap<File, BVRModel>();
+	static Map<BVRModel, File> loadedrev = new HashMap<BVRModel, File>();
+	static Map<BVRModel, List<IWorkbenchPart>> views = new HashMap<BVRModel, List<IWorkbenchPart>>();
 	
-	public static BVRModel getModel(File f){
-		BVRModel m;
-		if(loaded.containsKey(f)){
-			m = loaded.get(f);
-		}else{
-			m = new BVRModel(f);
-			loaded.put(f, m);
+	public static BVRModel getModel(File f, IWorkbenchPart iwp){
+		
+		if(!loaded.containsKey(f)){
+			BVRModel newm = new BVRModel(f);
+			loaded.put(f, newm);
+			loadedrev.put(newm, f);
+			views.put(newm, new ArrayList<IWorkbenchPart>());
 		}
 		
+		BVRModel m = loaded.get(f);
+		views.get(m).add(iwp);
+		
 		return m;
+	}
+
+	public static void editorClosed(IWorkbenchPart x) {
+		for(Entry<BVRModel, List<IWorkbenchPart>> z : views.entrySet()){
+			if(z.getValue().contains(x)){
+				z.getValue().remove(x);
+				if(z.getValue().size() == 0){
+					BVRModel m = z.getKey();
+					File f = loadedrev.get(m);
+					//System.out.println("Unloading " + m + ", " + f);
+					loaded.remove(f);
+					loadedrev.remove(m);
+				}
+			}
+		}
 	}
 }
