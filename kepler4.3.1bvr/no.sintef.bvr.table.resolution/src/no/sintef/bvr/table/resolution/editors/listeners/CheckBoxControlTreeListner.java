07b5a1603ffbb69bf4710b49449da7a25677748c
DaisukeShimbara <bangbangbang.org@gmail.com>
2014-02-11 15:11:48 +0100

    Fix null pointer exception by resolved*** without resolvedVSpec

--- kepler4.3.1bvr/no.sintef.bvr.table.resolution/src/no/sintef/bvr/table/resolution/editors/listeners/CheckBoxControlTreeListner.java
+++ kepler4.3.1bvr/no.sintef.bvr.table.resolution/src/no/sintef/bvr/table/resolution/editors/listeners/CheckBoxControlTreeListner.java
@@ -6,13 +6,14 @@ import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swt.widgets.TreeItem;
 
+import bvr.Choice;
 import bvr.ChoiceResolutuion;
+import bvr.VSpec;
 
 /**
- * @author daisuke
- * This listner handle the Check event of checkboxes on the tree.
- * If check event happen on ChoiceResolution, it change state.
- * Other case is ignored(set on -> set off ).
+ * @author daisuke This listner handle the Check event of checkboxes on the
+ *         tree. If check event happen on ChoiceResolution, it change state.
+ *         Other case is ignored(set on -> set off ).
  */
 public class CheckBoxControlTreeListner implements Listener {
 	private final Tree tree;
@@ -21,12 +22,21 @@ public class CheckBoxControlTreeListner implements Listener {
 		this.tree = tree;
 	}
 
-	
 	public void handleEvent(Event event) {
 		if (event.detail == SWT.CHECK) {
+
+			VSpec resolved = null;
+			if (event.item.getData() instanceof ChoiceResolutuion
+					&& ((ChoiceResolutuion) event.item.getData())
+							.getResolvedChoice() != null) {
+				resolved = ((ChoiceResolutuion) event.item.getData())
+						.getResolvedChoice();
+			} else {
+				resolved = ((ChoiceResolutuion) event.item.getData())
+						.getResolvedVSpec();
+			}
 			if (!(event.item.getData() instanceof ChoiceResolutuion)
-					|| ((event.item.getData() instanceof ChoiceResolutuion) && ((ChoiceResolutuion) event.item
-							.getData()).getResolvedChoice()
+					|| ((event.item.getData() instanceof ChoiceResolutuion) && ((Choice) resolved)
 							.isIsImpliedByParent())) {
 				event.detail = SWT.NONE;
 				event.type = SWT.None;
