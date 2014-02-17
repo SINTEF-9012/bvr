34de7987ab5c4d2d8d326885266b9b92317fc7eb
vassik <Anatoly.Vasilevskiy@sintef.no>
2014-02-17 13:23:43 +0100

    comments are added for vspec

--- kepler4.3.1bvr/no.sintef.bvr.model/src/bvr/VSpec.java
+++ kepler4.3.1bvr/no.sintef.bvr.model/src/bvr/VSpec.java
@@ -20,6 +20,7 @@ import org.eclipse.emf.common.util.EList;
  *   <li>{@link bvr.VSpec#getResolutionTime <em>Resolution Time</em>}</li>
  *   <li>{@link bvr.VSpec#getOwnedVSpecDerivation <em>Owned VSpec Derivation</em>}</li>
  *   <li>{@link bvr.VSpec#getChild <em>Child</em>}</li>
+ *   <li>{@link bvr.VSpec#getComment <em>Comment</em>}</li>
  * </ul>
  * </p>
  *
@@ -111,5 +112,31 @@ public interface VSpec extends VPackageable {
 	 * @generated
 	 */
 	EList<VSpec> getChild();
+
+	/**
+	 * Returns the value of the '<em><b>Comment</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Comment</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Comment</em>' attribute.
+	 * @see #setComment(String)
+	 * @see bvr.BvrPackage#getVSpec_Comment()
+	 * @model
+	 * @generated
+	 */
+	String getComment();
+
+	/**
+	 * Sets the value of the '{@link bvr.VSpec#getComment <em>Comment</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Comment</em>' attribute.
+	 * @see #getComment()
+	 * @generated
+	 */
+	void setComment(String value);
 
 } // VSpec
