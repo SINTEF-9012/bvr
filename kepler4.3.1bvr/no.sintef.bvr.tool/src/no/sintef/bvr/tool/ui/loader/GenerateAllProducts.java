c3f4f24b780c83e052ac94d35bc8ce2776bc8d8c
vassik <Anatoly.Vasilevskiy@sintef.no>
2014-03-28 14:54:32 +0100

    generate all product fix

--- kepler4.3.1bvr/no.sintef.bvr.tool/src/no/sintef/bvr/tool/ui/loader/GenerateAllProducts.java
+++ kepler4.3.1bvr/no.sintef.bvr.tool/src/no/sintef/bvr/tool/ui/loader/GenerateAllProducts.java
@@ -9,7 +9,15 @@ import java.util.List;
 import javax.swing.JOptionPane;
 import javax.swing.JTabbedPane;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.emf.common.util.EList;
+
+import bvr.VSpecResolution;
 import de.ovgu.featureide.fm.core.io.UnsupportedModelException;
+import no.sintef.bvr.tool.common.Constants;
 import no.sintef.bvr.tool.context.Context;
 import no.sintef.bvr.tool.ui.context.StaticUICommands;
 import no.sintef.ict.splcatool.CNF;
@@ -20,7 +28,6 @@ import no.sintef.ict.splcatool.GUIDSL;
 import no.sintef.ict.splcatool.GraphMLFM;
 
 public class GenerateAllProducts implements ActionListener {
-	private int t;
 	private BVRModel m;
 	private BVRView v;
 	
@@ -31,21 +38,26 @@ public class GenerateAllProducts implements ActionListener {
 
 
 	public void actionPerformed(ActionEvent ae) {
-		
-		try {
-			GUIDSL gdsl = m.getBVRM().getGUIDSL();
-			CNF cnf = gdsl.getSXFM().getCNF();
-			CoveringArray ca = new CoveringArrayComplete(cnf);
-			ca.generate();
-			GraphMLFM gfm = gdsl.getGraphMLFMConf(ca);
-			m.getBVRM().getCU().getOwnedVSpecResolution().clear();
-			m.getBVRM().getChoiceResolutions(gfm);
-		} catch (Exception e) {
-			Context.eINSTANCE.logger.error("Generating covering array failed:", e);
-			StaticUICommands.showMessageErrorDialog(null, e, "Generating covering array failed:");
-			return;
-		}
-		
-		//v.notifyResolutionViewUpdate();
+		Job job = new Job("Generating all possible products") {	
+			 @Override
+			 protected IStatus run(IProgressMonitor monitor) {
+				 try {
+					GUIDSL gdsl = m.getBVRM().getGUIDSL();
+					CNF cnf = gdsl.getSXFM().getCNF();
+					CoveringArray ca = new CoveringArrayComplete(cnf);
+					ca.generate();
+					GraphMLFM gfm = gdsl.getGraphMLFMConf(ca);
+					Context.eINSTANCE.getEditorCommands().removeOwnedVSpecResolutions(m.getBVRM().getCU());
+					EList<VSpecResolution> resolutions = m.getBVRM().getChoiceResolutions(gfm);
+					Context.eINSTANCE.getEditorCommands().addOwnedVSpecResolutionsConfigurableUnit(m.getBVRM().getCU(), resolutions);
+				 } catch(Exception e){
+					 Context.eINSTANCE.logger.error("Generating all possible products failed:", e);
+					 Status status = new Status(Status.ERROR, Constants.PLUGIN_ID, "Generating all possible products failed (see log for more details): " + e.getMessage(), e);
+					 return status;
+				 }
+				 return Status.OK_STATUS;
+			 }
+		};
+		job.schedule();
 	}
 }
