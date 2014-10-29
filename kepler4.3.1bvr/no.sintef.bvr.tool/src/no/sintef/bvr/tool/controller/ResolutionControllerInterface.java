9ea335ad821999ac8dfd342feb5a9c87a44465cc
mbholos <martin.b.o.holos@gmail.com>
2014-10-29 02:41:28 +0100

    newLine fix

--- kepler4.3.1bvr/no.sintef.bvr.tool/src/no/sintef/bvr/tool/controller/ResolutionControllerInterface.java
+++ kepler4.3.1bvr/no.sintef.bvr.tool/src/no/sintef/bvr/tool/controller/ResolutionControllerInterface.java
@@ -3,4 +3,7 @@ package no.sintef.bvr.tool.controller;
 public interface ResolutionControllerInterface <GUI_NODE, MODEL_OBJECT>{
 
 	public void addChoiceResolution(GUI_NODE parent, MODEL_OBJECT resolvedVSpec);
+	
+	
+	
 }
