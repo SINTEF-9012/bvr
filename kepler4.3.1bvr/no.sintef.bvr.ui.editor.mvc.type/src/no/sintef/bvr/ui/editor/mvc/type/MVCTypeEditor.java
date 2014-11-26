34550520ba564579a964c647f662dc48382271fc
vassik <Anatoly.Vasilevskiy@sintef.no>
2014-11-26 17:03:30 +0100

    plugin for the type editor

---
+++ kepler4.3.1bvr/no.sintef.bvr.ui.editor.mvc.type/src/no/sintef/bvr/ui/editor/mvc/type/MVCTypeEditor.java
@@ -0,0 +1,25 @@
+package no.sintef.bvr.ui.editor.mvc.type;
+
+import no.sintef.bvr.ui.editor.common.MVCEditor;
+
+public class MVCTypeEditor extends MVCEditor {
+
+	@Override
+	public void setTitle() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setContents() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void createView() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
