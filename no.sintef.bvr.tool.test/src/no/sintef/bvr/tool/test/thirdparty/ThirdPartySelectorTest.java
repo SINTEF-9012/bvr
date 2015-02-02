58482c00913367c26a27dd3fa371870096e32097
vassik <Anatoly.Vasilevskiy@sintef.no>
2015-02-02 20:53:18 +0100

    improved test

--- no.sintef.bvr.tool.test/src/no/sintef/bvr/tool/test/thirdparty/ThirdPartySelectorTest.java
+++ no.sintef.bvr.tool.test/src/no/sintef/bvr/tool/test/thirdparty/ThirdPartySelectorTest.java
@@ -11,6 +11,7 @@ import no.sintef.bvr.tool.context.ThirdpartyEditorSelector;
 import no.sintef.bvr.tool.test.Activator;
 
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
@@ -20,7 +21,9 @@ import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.part.FileEditorInput;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import bvr.BVRModel;
@@ -28,11 +31,12 @@ import bvr.BvrPackage;
 
 public class ThirdPartySelectorTest {
 	
-	private TestProject testProject;
-	private ModelSelector selector;
-	private FileEditorInput vmFileinput;
-	private IEditorPart thirdpartyEditor;
-	private IWorkbenchPage activePage;
+	private static TestProject testProject;
+	private static ModelSelector selector;
+	private static FileEditorInput vmFileinput;
+	private static IEditorPart thirdpartyEditor;
+	private static IWorkbenchPage activePage;
+	private static BVRModel model;
 
 	
 	private static String[] testFolders = {
@@ -45,8 +49,8 @@ public class ThirdPartySelectorTest {
 		
 	};
 
-	@Before
-	public void setUp() throws Exception {
+	@BeforeClass
+	public static void setUp() throws Exception {
 		testProject = new TestProject("ThirdPartySelector1", Activator.PLUGIN_ID);
 		testProject.createFolders(testFolders);
 		testProject.createResources(testResources);
@@ -64,30 +68,46 @@ public class ThirdPartySelectorTest {
 		ResourceSet resSet = new ResourceSetImpl();
 		URI uri = URI.createPlatformResourceURI("/"+ testProject.getName() + testResources[0].getTarget(), true);
 		Resource resource = resSet.getResource(uri, true);
-		BVRModel model = (BVRModel) resource.getContents().get(0);
-	
-		//select element
-		thirdpartyEditor.getSite().getSelectionProvider().setSelection(new StructuredSelection(model));
+		model = (BVRModel) resource.getContents().get(0);
 		
 		//set up selector
 		ThirdpartyEditorSelector.setWorkbeach(testProject.getActionWorkbenchWindow());
 		selector = ThirdpartyEditorSelector.getEditorSelector();
 	}
 
-	@After
-	public void tearDown() throws Exception {
+	@AfterClass
+	public static void tearDown() throws Exception {
 		thirdpartyEditor.dispose();
 		testProject.disposeTestProject();
 	}
 
 	/**
-	 * Assertion: If we select one element in a tree edito then exactly one element should be returned as selected
+	 * Assertion: If we select one element in a tree editor then exactly one element should be returned as selected
 	 */
 	@Test
-	public void testSelection() {
+	public void testNotEmpySelection() {
+		
+		//select element
+		thirdpartyEditor.getSite().getSelectionProvider().setSelection(new StructuredSelection(model));
 		List<Object> selection = selector.getSelections();
-	
 		assertTrue("Selection is made, but no object returned", selection.size() == 1);
+		
+		EObject eObject = selector.getEObject(selection.get(0));
+		assertTrue("Selected element doesn not correspond to the made selection", eObject.equals(model));
+	}
+	
+	/**
+	 * Assertion: If am empty selection is made then no elements are returned
+	 */
+	@Test
+	public void testEmptySelection() {
+		
+		//select element
+		thirdpartyEditor.getSite().getSelectionProvider().setSelection(new StructuredSelection());
+		List<Object> selection = selector.getSelections();
+		assertTrue("Not empty selection", selection.size() == 0);
+		
+		
 	}
 
 }
