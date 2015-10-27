f5ccfcf3903ceaf383ee229e9f15cf9d100f18a8
vassik <Anatoly.Vasilevskiy@sintef.no>
2015-10-27 13:10:18 +0100

    some formating

--- no.sintef.bvr.tool/src/no/sintef/bvr/tool/ui/command/event/ExecuteResolutionEvent.java
+++ no.sintef.bvr.tool/src/no/sintef/bvr/tool/ui/command/event/ExecuteResolutionEvent.java
@@ -9,6 +9,7 @@ package no.sintef.bvr.tool.ui.command.event;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.swing.JFileChooser;
@@ -40,8 +41,17 @@ public class ExecuteResolutionEvent implements ActionListener {
 
 		List<String> bm_files_paths = controller.getResolutionControllerInterface().getBaseModelFiles();
 
+		String file_list = "[";
+		Iterator<String> iterator = bm_files_paths.iterator();
+		while (iterator.hasNext()) {
+			file_list += iterator.next();
+			if (iterator.hasNext())
+				file_list += ",\n";
+		}
+		file_list += "]";
+
 		boolean result = StaticUICommands.showConfirmationDialog(Context.eINSTANCE.getActiveJApplet(), "Is it a correct base model?",
-				"Base model consists of the following files:\n " + bm_files_paths
+				"Base model consists of the following files:\n " + file_list
 						+ "\nIf the base model is incorrect, select base model files in the Eclipse Navigator View (Window->Show View->Navigator).");
 
 		if (!result)
